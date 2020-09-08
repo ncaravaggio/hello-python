01cdba30aaafdee333d62c0abe7d91b895161076
Nick Caravaggio <47163134+ncaravaggio@users.noreply.github.com>
2020-09-08 11:36:56 -0700

    project1

---
+++ heropickrates.py
@@ -0,0 +1,43 @@
+#Nick Caravaggio
+#ncaravaggio
+#C02617510
+
+overwatch_top_hero_pickrates_string = '''
+Hero           Pickrate          Winrate
+-----------------------------------------
+Ana             11.92%            50.10%
+Zarya            8.55%            52.30%
+Reinhardt        8.08%            51.14%
+Mercy            7.17%            51.94%
+Roadhog          6.74%            51.22%
+Moira            6.34%            49.52%
+Ashe             5.40%            52.90%
+Genji            4.60%            49.84%
+Mccree           4.08%            46.31%
+Sigma            3.22%            50.50%
+'''
+
+overwatch_top_hero_pickrates_list =
+['Ana', 11.92, 50.10, 'Zarya', 8.55, 52.30, 'Reinhardt', 8.08, 51.14, 7.17,
+  'Mercy', 7.17, 51.94, 'Roadhog', 6.74, 51.22, 'Moira', 6.34, 49.52,
+  'Ashe', 5.40, 52.90, 'Genji', 4.60, 49.84, 'Mccree', 4.08, 46.31,
+  'Sigma', 3.22, 50.50]
+
+class OverwatchTopHeroPickratesClass
+{
+    overwatch_top_hero_pickrates_string_of_class = '''
+Hero           Pickrate          Winrate
+-----------------------------------------
+Ana             11.92%            50.10%
+Zarya            8.55%            52.30%
+Reinhardt        8.08%            51.14%
+Mercy            7.17%            51.94%
+Roadhog          6.74%            51.22%
+Moira            6.34%            49.52%
+Ashe             5.40%            52.90%
+Genji            4.60%            49.84%
+Mccree           4.08%            46.31%
+Sigma            3.22%            50.50%
+'''
+    return overwatch_top_hero_pickrates_string_of_class
+}
